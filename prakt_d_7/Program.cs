2867dc69913de76f7103653520154cacc8331f8d
Winter <nice@email.zip>
2023-10-23 16:07:20 +0300

    stuffs

--- prakt_d_7/Program.cs
+++ prakt_d_7/Program.cs
@@ -17,12 +17,13 @@ namespace prakt_d_7
 
 
             bool exit = false;
-            while (exit==false)
+            while (exit == false)
             {
                 Console.Write("Ievadies skaitli no 1 līdz 5 vai IZIET lai beigtu darbību");
                 string darbiba = Console.ReadLine();
 
-                switch (darbiba) {
+                switch (darbiba)
+                {
                     case "1":
                         Console.WriteLine("Viens");
                         break;
@@ -43,43 +44,42 @@ namespace prakt_d_7
                         exit = true;
                         break;
                 }
+            }
+            // 2. uzd
+            Console.WriteLine("=== 2. uzdevums ===");
 
-                // 2. uzd
-                Console.WriteLine("=== 2. uzdevums ===");
-
-                Console.Write("Cik reizes pārbaudīt skaitļus? ");
-                int reizes = int.Parse(Console.ReadLine());
-                for(int i = 0; i < reizes; i++)
+            Console.Write("Cik reizes pārbaudīt skaitļus? ");
+            int reizes = int.Parse(Console.ReadLine());
+            for(int i = 0; i < reizes; i++)
+            {
+                Console.Write("Ievadiet skaitli: ");
+                int skaitlis = int.Parse(Console.ReadLine());
+                if (skaitlis%3 != 0)
                 {
-                    Console.Write("Ievadiet skaitli: ");
-                    int skaitlis = int.Parse(Console.ReadLine());
-                    if (skaitlis%3 != 0)
-                    {
-                        Console.WriteLine("Skaitlis NEdalās ar 3 bez atlikuma!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Skaitlis dalās ar 3 bez atlikuma!");
-                    }
+                    Console.WriteLine("Skaitlis NEdalās ar 3 bez atlikuma!");
                 }
-
-                // 3. uzd
-                Console.WriteLine("=== 3. uzdevums ===");
-                Console.WriteLine("Ievadiet trīsstūra platumu");
-                int platums = int.Parse(Console.ReadLine());
-                int r = 0;
-
-                while (r<=platums)
+                else
                 {
-                    int j = 0;
-                    while (j<=r)
-                    {
-                        Console.Write("*");
-                        j++;
-                    }
-                    Console.Write("\n");
-                    r++;
+                    Console.WriteLine("Skaitlis dalās ar 3 bez atlikuma!");
                 }
+            }
+
+            // 3. uzd
+            Console.WriteLine("=== 3. uzdevums ===");
+            Console.Write("Ievadiet trīsstūra platumu: ");
+            int platums = int.Parse(Console.ReadLine());
+            int spaces_left = 0;
+            //int spaces_right = 0;
+            int asterisk = platums;
+            while (asterisk > 0)
+            {
+                Console.Write(new string(' ', spaces_left));
+                Console.Write(new string('*', asterisk));
+                //Console.Write(new string(' ', spaces_right));
+                Console.Write('\n');
+                asterisk -= 2;
+                spaces_left++;
+                //spaces_right++;
             }
         }
     }
