e8a825790a01c4a41e5e081be8f27deb4f9cba7c
Winter <nice@email.zip>
2023-12-05 21:36:18 +0200

    stuffs

---
+++ prakt_d_14/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt_d_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1 - atņemšana\n2 - saskaitīšana\n3 - reizināšana\n4 - dalīšana\n0 - iziet");
+            int decision = int.Parse(Console.ReadLine());
+
+            switch (decision)
+            {
+                case 0:
+                    break;
+                case 1:
+                    sum();
+                    break;
+            }
+        }
+
+        static void sum()
+        {
+            Console.Write("Cik piemērus rēķināsiet: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < count; i++)
+            {
+                
+            }
+        }
+
+        static void sub()
+        {
+
+        }
+
+        static void div()
+        {
+
+        }
+
+        static void mult()
+        {
+
+        }
+    }
+}
