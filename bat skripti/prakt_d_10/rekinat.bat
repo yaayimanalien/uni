c0fbd43bb0708e2bfdea75fae09e02e66ad1b1f6
Winter <nice@email.zip>
2023-12-06 13:49:36 +0200

    stuffs

--- bat skripti/prakt_d_10/rekinat.bat
+++ bat skripti/prakt_d_10/rekinat.bat
@@ -1,5 +1,4 @@
 @echo off
-setlocal enabledelayedexpansion
 
 if %parametrs% == + set /a rez=%skaitlis1%+%skaitlis2%
 if %parametrs% == - set /a rez=%skaitlis1%-%skaitlis2%
@@ -9,18 +8,17 @@ if %parametrs% == 2 goto kapinsana
 :h
 (
 echo Rezultats:
-echo %skaitlis1%%parametrs%%skaitlis2%=%rez%
+if %parametrs% == 2 (echo %skaitlis1%^^2=%rez%) else (echo %skaitlis1%%parametrs%%skaitlis2%=%rez%)
 ) > log.txt
 
 type log.txt
-
 echo Aprekinatais rezultats ir: %rez%
 
 if %rez% GEQ 10 goto divcipari
 goto viencipari
 :divcipari
-set firstCharacter=!rez:~0,1!
-set secondCharacter=!rez:~1,1!
+set firstCharacter=%rez:~0,1%
+set secondCharacter=%rez:~1,1%
 echo 1. cipars %firstCharacter%
 echo 2. cipars %secondCharacter%
 goto beigas
@@ -29,8 +27,6 @@ echo nav divciparu skaitlis
 goto beigas
 :kapinsana
 set /a rez=%skaitlis1%*%skaitlis1%
-set %parametrs%=^^
-set %skaitlis2%=2
 goto h
 :beigas
 REM Reinis Jānis Barons DvDz 3
