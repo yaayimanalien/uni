11eaf76da1da9374742c79fcf176d5d81d4f6388
Winter <nice@email.zip>
2023-12-10 21:03:11 +0200

    stuffs

--- prakt_d_14/Program.cs
+++ prakt_d_14/Program.cs
@@ -10,43 +10,117 @@ namespace prakt_d_14
     {
         static void Main(string[] args)
         {
-            Console.Write("1 - atņemšana\n2 - saskaitīšana\n3 - reizināšana\n4 - dalīšana\n0 - iziet");
-            int decision = int.Parse(Console.ReadLine());
 
-            switch (decision)
+            Random random = new Random();
+            bool loopStop = false;
+
+            while (loopStop == false)
             {
-                case 0:
-                    break;
-                case 1:
-                    sum();
-                    break;
+                Console.Write("1 - atņemšana\n2 - saskaitīšana\n3 - reizināšana\n4 - dalīšana\n0 - iziet\n");
+                int decision = int.Parse(Console.ReadLine());
+
+                switch (decision)
+                {
+                    case 0:
+                        loopStop = true;
+                        break;
+                    case 1:
+                        sub(random.Next(1, 10), random.Next(1, 10));
+                        break;
+                    case 2:
+                        sum(random.Next(1, 10), random.Next(1, 10));
+                        break;
+                    case 3:
+                        mult(random.Next(1, 10), random.Next(1, 10));
+                        break;
+                    case 4:
+                        div(random.Next(1, 10), random.Next(1, 10));
+                        break;
+                }
+
             }
         }
 
-        static void sum()
+        static void sub(int sk1, int sk2)
         {
             Console.Write("Cik piemērus rēķināsiet: ");
             int count = int.Parse(Console.ReadLine());
 
-            for (int i = 1; i < count; i++)
+            for (int i = 1; i <= count; i++)
             {
-                
+                Console.Write($"{sk1} - {sk2} = ");
+                int input = int.Parse(Console.ReadLine());
+                if (sk1-sk2==input)
+                {
+                    Console.WriteLine("Rezultāts ievadīts pareizi");
+                }
+                else
+                {
+                    Console.WriteLine("Rezultāts ievadīts nepareizi");
+                }
+            }
+
+        }
+
+        static void sum(int sk1, int sk2)
+        {
+            Console.Write("Cik piemērus rēķināsiet: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write($"{sk1} - {sk2} = ");
+                int input = int.Parse(Console.ReadLine());
+                if (sk1 - sk2 == input)
+                {
+                    Console.WriteLine("Rezultāts ievadīts pareizi");
+                }
+                else
+                {
+                    Console.WriteLine("Rezultāts ievadīts nepareizi");
+                }
             }
         }
 
-        static void sub()
-        {
 
+        static void div(int sk1, int sk2)
+        {
+            Console.Write("Cik piemērus rēķināsiet: ");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write($"{sk1} / {sk2} = ");
+                int input = int.Parse(Console.ReadLine());
+                if (sk1 / sk2 == input)
+                {
+                    Console.WriteLine("Rezultāts ievadīts pareizi");
+                }
+                else
+                {
+                    Console.WriteLine("Rezultāts ievadīts nepareizi");
+                }
+            }
         }
 
-        static void div()
+        static void mult(int sk1, int sk2)
         {
+            Console.Write("Cik piemērus rēķināsiet: ");
+            int count = int.Parse(Console.ReadLine());
 
-        }
-
-        static void mult()
-        {
-
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write($"{sk1} * {sk2} = ");
+                int input = int.Parse(Console.ReadLine());
+                if (sk1 * sk2 == input)
+                {
+                    Console.WriteLine("Rezultāts ievadīts pareizi");
+                }
+                else
+                {
+                    Console.WriteLine("Rezultāts ievadīts nepareizi");
+                }
+            }
         }
     }
 }
