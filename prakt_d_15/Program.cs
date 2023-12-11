8c538a8dfe8bd9889b19996ce37b811ec3412821
Winter <nice@email.zip>
2023-12-11 10:39:07 +0200

    stuffs

---
+++ prakt_d_15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt_d_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool breakLoop = false;
+            while (breakLoop == false)
+            {
+                Console.WriteLine("0 - iziet\n1 - Budžeta pamatsummas ievadīšana\n2 - budžeta noguldīšana\n3 - budžeta pamatsummas palielināšana\n5 - budžeta samazinājums\n\nIzvēlies darbību: ");
+                string decision = Console.ReadLine();
+                switch (decision)
+                {
+                    case "0":
+                        breakLoop = true;
+                        break;
+                }
+            }
+        }
+
+        static void procenti()
+        {
+            // iepriekš kkad tika darīts
+        }
+
+        static void pluss()
+        {
+
+        }
+
+        static void minuss()
+        {
+
+        }
+    }
+}
