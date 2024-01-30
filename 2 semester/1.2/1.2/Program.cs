c49ac320bbafb3435771ee5d05e8b28aec81d3af
winter <winter@is.me>
2024-01-30 15:46:02 +0200

    stuffs

---
+++ 2 semester/1.2/1.2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _1._2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 2. uzd
+            Console.WriteLine("=== 2. uzdevums ===");
+
+            string word = "algoritms";
+            bool loopStop = false;
+            int tries = 0;
+
+            bool[] guessedLetters = new bool[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                guessedLetters[i] = false;
+            }
+            
+            while (loopStop == false)
+            {
+                tries++;
+                int correctLetters = 0;
+                string displayWord = "";
+                Console.Write("\n Ievadi burtu: ");
+                string letter = Console.ReadLine();
+                
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (letter == char.ToString(word[i]))
+                    {
+                        guessedLetters[i] = true;
+                    }
+
+                    if (guessedLetters[i])
+                    {
+                        displayWord += word[i];
+                    }
+                    else
+                    {
+                        displayWord += "_";
+                    }
+                }
+                
+                Console.WriteLine(displayWord);
+                
+                for (int i = 0; i < guessedLetters.Length; i++)
+                {
+                    if (guessedLetters[i] == true)
+                    {
+                        correctLetters++;
+                    }
+                }
+                if (correctLetters == guessedLetters.Length)
+                {
+                    Console.WriteLine($"Vārds uzminēts pareizi ar mēģinājumu skaitu {tries}");
+                    loopStop = true;
+                }
+            }
+        }
+    }
+}
---
+++ 2 semester/1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1. uzd
+            Console.WriteLine("=== 1. uzdevums ===");
+            
+            bool loopStop = false;
+            Random random = new Random();
+            int randomInt = random.Next(1, 100);
+            int count = 0;
+
+            while (loopStop == false)
+            {
+                count++;
+                Console.Write("Ievadi skaitli: ");
+                int input = int.Parse(Console.ReadLine());
+                if (input>randomInt)
+                {
+                    Console.WriteLine("Mazāks");
+                }
+                else if (input<randomInt)
+                {
+                    Console.WriteLine("Lielāks");
+                }
+                else if (input == randomInt)
+                {
+                    Console.WriteLine("Skaitlis ir uzminēts!");
+                    Console.WriteLine($"Reizes: {count}");
+                    loopStop = true;
+                }
+                
+                
+                
+            }
+            
+            
+        }
+    }
+}
