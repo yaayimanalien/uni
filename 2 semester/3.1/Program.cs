233d3f38395518b0753c52c11fce28b849e14a6a
winter <winter@is.me>
2024-02-06 20:47:44 +0200

    Programming is fun 😊

---
+++ 2 semester/3.1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _3._1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1. uzd
+            Console.WriteLine("=== 1. uzdevums ===");
+            double[] arr1 = new double[10];
+            double[] arr2 = new double[10];
+            Random random = new Random();
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = random.Next(1, 9);
+                Console.Write($"{arr1[i]}\t");
+            }
+
+            Console.Write("\n");
+            
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr2[i] = random.Next(1, 9);
+                Console.Write($"{arr2[i]}\t");
+            }
+            Console.Write("\n");
+            
+            bool stop = false;
+
+            while (stop == false)
+            {
+                Console.WriteLine("1 - Summēt\n2 - Atņemt\n3 - Reizināt\n4 - Dalīt\n5 - Vid aritm\n0 - iziet");
+                Console.Write("Jūsu izvēle: ");
+                int decision = int.Parse(Console.ReadLine());
+
+                switch (decision)
+                {
+                    case 0:
+                        stop = true;
+                        break;
+                    case 1:
+                        Summet(arr1, arr2);
+                        break;
+                    case 2:
+                        Atnemt(arr1, arr2);
+                        break;
+                    case 3:
+                        Reizinat(arr1, arr2);
+                        break;
+                    case 4:
+                        Dalit(arr1, arr2);
+                        break;
+                    case 5:
+                        VidejaisAritmetiskais(arr1, arr2);
+                        break;
+                }
+            }
+        }
+
+        public static void Summet(double[] double1, double[] double2)
+        {
+            double[] arr3 = new double[10];
+            
+            Console.Write("Rezultāts: ");
+            for (int i = 0; i < double1.Length; i++)
+            {
+                arr3[i] = double1[i] + double2[i];
+            }
+            for (int i = 0; i < double1.Length; i++)
+            {
+                Console.Write($"{arr3[i]} ");
+            }
+            Console.Write("\n");
+        }
+        
+        public static void Atnemt(double[] double1, double[] double2)
+        {
+            double[] arr3 = new double[10];
+            
+            Console.Write("Rezultāts: ");
+            for (int i = 0; i < double1.Length; i++)
+            {
+                arr3[i] = double1[i] - double2[i];
+                Console.Write($"{arr3[i]} ");
+            }
+            Console.Write("\n");
+        }
+        
+        public static void Reizinat(double[] double1, double[] double2)
+        {
+            double[] arr3 = new double[10];
+            
+            Console.Write("Rezultāts: ");
+            for (int i = 0; i < double1.Length; i++)
+            {
+                arr3[i] = double1[i] * double2[i];
+                Console.Write($"{arr3[i]} ");
+            }
+            Console.Write("\n");
+        }
+        
+        public static void Dalit(double[] double1, double[] double2)
+        {
+            double[] arr3 = new double[10];
+            
+            Console.Write("Rezultāts: ");
+            for (int i = 0; i < double1.Length; i++)
+            {
+                arr3[i] = double1[i] / double2[i];
+                Console.Write($"{arr3[i]} ");
+            }
+            Console.Write("\n");
+        }
+
+        public static void VidejaisAritmetiskais(double[] double1, double[] double2)
+        {
+            double aritm1 = 0;
+            double aritm2 = 0;
+            
+            for (int i = 0; i < double1.Length; i++)
+            {
+                aritm1 += double1[i];
+                aritm2 += double2[i];
+            }
+
+            aritm1 = aritm1 / (double1.Length);
+            aritm2 = aritm2 / (double2.Length);
+            
+            Console.WriteLine($"1. masīva vidējā aritmētiskā vērtība: {aritm1}");
+            Console.WriteLine($"2. masīva vidējā aritmētiskā vērtība: {aritm2}");
+            if (aritm1>aritm2)
+            {
+                Console.WriteLine($"1. masīva vid vērtība ir lielāka par 2. masīva vid vērtību par {aritm1-aritm2}");
+            }
+            if (aritm2 > aritm1)
+            {
+                Console.WriteLine($"1. masīva vid vērtība ir lielāka par 2. masīva vid vērtību par {aritm1-aritm2}");
+            }
+            Console.WriteLine($"Abu masīvu vidējā aritmētiskā vērtība: {(aritm1 + aritm2) / (double1.Length*2)}");
+        }
+    }
+}
---
+++ 2 semester/3.2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace _3._2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ievadi masīva rindu skaitu: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Ievadi masīva kolonnu skaitu: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] arr1 = new int[m, n];
+
+            InitMas(arr1);
+            PrintMas(arr1);
+            Console.WriteLine($"Masīva min vērtība: {MinMas(arr1)}");
+            Console.WriteLine($"Masīva max vērtība: {MaxMas(arr1)}");
+            
+            Console.Write("Ievadiet skaitli kuru meklēt: ");
+            Console.WriteLine($"Skaitlis masīvā atkārtojas {Meklet(arr1, int.Parse(Console.ReadLine()))} reizes");
+        }
+        
+        private static int[,] InitMas(int[,] input)
+        {
+            Random random = new Random();
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    input[i, j] = random.Next(1, 9);
+                }
+            }
+            return input;
+        }
+
+        private static void PrintMas(int[,] input)
+        {
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    Console.Write($"{input[i,j]} ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        private static int MinMas(int[,] input)
+        {
+            int min = 10;
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (min>input[i,j])
+                    {
+                        min = input[i, j];
+                    }
+                }
+            }
+
+            return min;
+        }
+
+        private static int MaxMas(int[,] input)
+        {
+            int max = 1;
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (max < input[i,j])
+                    {
+                        max = input[i, j];
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        private static int Meklet(int[,] input, int query)
+        {
+            int count = 0;
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if(input[i,j]==query)
+                    {
+                        count++;
+                    }
+                }
+            }
+            
+            return count;
+        }
+    }
+}
