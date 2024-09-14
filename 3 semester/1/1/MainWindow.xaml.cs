157914fb1e6ac6440d4e90f939750a9e9b85d982
yaayimanalien <51403842+yaayimanalien@users.noreply.github.com>
2024-09-14 16:12:45 +0300

    new stuff

---
+++ 3 semester/1/1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string? action = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            action = "add";
+        }
+
+        private void subtract_Click(object sender, RoutedEventArgs e)
+        {
+            action = "subtract";
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+            action = "multiply";
+        }
+
+        private void subdivide_Click(object sender, RoutedEventArgs e)
+        {
+            action = "subdivide";
+        }
+
+        private void equal_Click(object sender, RoutedEventArgs e)
+        {
+            if (action == null)
+            {
+                MessageBox.Show("Lūdzu izvēlies darbību!");
+            }
+            else
+            {
+                if (firstNumber==null | secondNumber==null)
+                {
+                    MessageBox.Show("Lūdzu ierakstiet skaitļus!");
+                }
+                else
+                {
+                    bool a;
+                    bool b;
+                    int rand = int.TryParse(firstNumber.Text, out a);
+
+                    if ()
+                    {
+                        switch (action)
+                        {
+                            case "sum":
+                                answer.Text = (double.Parse(firstNumber.Text) + double.Parse(secondNumber.Text)).ToString();
+                                break;
+                            case "subtract":
+                                answer.Text = (double.Parse(firstNumber.Text) - double.Parse(secondNumber.Text)).ToString();
+                                break;
+                            case "multiply":
+                                answer.Text = (double.Parse(firstNumber.Text) * double.Parse(secondNumber.Text)).ToString();
+                                break;
+                            case "divide":
+                                answer.Text = (double.Parse(firstNumber.Text) / double.Parse(secondNumber.Text)).ToString();
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 3 semester/1_/f/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace f
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string? action = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            action = "add";
+        }
+
+        private void equal_Click(object sender, RoutedEventArgs e)
+        {
+            if (action != null & firstNumber.Text != null & secondNumber.Text != null)
+            {
+                double? result = null;
+                switch (action)
+                {
+                    case "add":
+                        result = double.Parse(firstNumber.Text) + double.Parse(secondNumber.Text);
+                        break;
+                    case "subtract":
+                        result = double.Parse(firstNumber.Text) - double.Parse(secondNumber.Text);
+                        break;
+                    case "multiply":
+                        result = double.Parse(firstNumber.Text) * double.Parse(secondNumber.Text);
+                        break;
+                    case "subdivide":
+                        result = double.Parse(firstNumber.Text) / double.Parse(secondNumber.Text);
+                        break;
+                }
+                answer.Text = result.ToString();
+            }
+            else 
+            {
+                //MessageBox.Show("Lūdzu ierakstiet skaitļus un izvēlieties darbību!");
+                answer.Text = "Lūdzu ierakstiet skaitļus un izvēlieties darbību!";
+            }
+
+        }
+
+        private void subtract_Click(object sender, RoutedEventArgs e)
+        {
+            action = "subtract";
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+            action = "multiply";
+        }
+
+        private void subdivide_Click(object sender, RoutedEventArgs e)
+        {
+            action = "subdivide";
+        }
+
+        private void firstNumber_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (firstNumber.Text == "Enter first number")
+            {
+                firstNumber.Text = "";
+            }
+            //else
+            //{
+            //    firstNumber.SelectAll();
+            //}
+        }
+
+        private void secondNumber_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (secondNumber.Text == "Enter second number")
+            {
+                secondNumber.Text = "";
+            }
+            //else
+            //{
+            //    secondNumber.SelectAll();
+            //}
+        }
+    }
+}
